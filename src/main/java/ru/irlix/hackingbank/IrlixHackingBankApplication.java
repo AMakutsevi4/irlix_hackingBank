f2173220494a0b8011588d2c7d35f2fe2c85d2ec
Alexandr <amakutsevich@bk.ru>
2024-07-28 14:36:27 +0700

    init commit

---
+++ src/main/java/ru/irlix/hackingbank/IrlixHackingBankApplication.java
@@ -0,0 +1,13 @@
+package ru.irlix.hackingbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrlixHackingBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrlixHackingBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/irlix/hackingbank/IrlixHackingBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.irlix.hackingbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrlixHackingBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
